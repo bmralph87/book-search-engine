9c5e7702b941a5903c6d615630281f454f43cadf
Britt Ralph <brittney.mr22@gmail.com>
2021-01-15 10:43:54 -0500

    backend-draft

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+type Query {
+  me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(thoughtText: String!): User
+    removeBook(bookId: ID!, reactionBody: String!): User
+  }
+
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  saved Books: [Book]
+}
+
+type Book {
+  bookId: ID
+  authors: String
+  description: String
+  title: String
+  image: Int
+  link: []
+}
+
+type Reaction {
+  _id: ID
+  reactionBody: String
+  createdAt: String
+  username: String
+}
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+
+// export the typeDefs
+module.exports = typeDefs;

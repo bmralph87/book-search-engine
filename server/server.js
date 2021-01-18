5f74c55b450aeb18180086ecbf1d41c285148c6b
Britt Ralph <brittney.mr22@gmail.com>
2021-01-18 00:20:09 -0500

    packages

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
